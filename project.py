2ee6b93d0fac483910d67afab5881c50fc26ac6f
yeyushenfan <tl2866@columbia.edu>
2018-11-10 20:15:52 +0000

    just testing

--- project.py
+++ project.py
@@ -6,3 +6,11 @@ import pandas as pd
 ticker = input("What stock would you like to analyse? ")
 print ("You would like to analyze:  " + ticker)
 
+
+
+import requests
+from bs4 import BeautifulSoup
+    
+response = requests.get(url)
+if not response.status_code == 200:
+    return output_list
