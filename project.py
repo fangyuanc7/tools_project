aeb2bfcf4457590e62f412a544813d4ca931632f
Kyle Liu <kkl2129@columbi.edu>
2018-11-18 00:51:25 +0000

    Add few comments for later work

--- project.py
+++ project.py
@@ -6,8 +6,9 @@ import datetime
 #%matplotlib inline
 import pandas_datareader.data as web
 import fix_yahoo_finance as yf
+#import googlefinance.client
 
-ticker = input("What stock would you like to analyse? ")
+ticker = input("What equity would you like to analyse? ")
 start = input("When would you like to begin analyzing? Please enter date in format YYYY/MM/DD: ")
 end = input("When would you like to end analyzing? Please enter date in format YYYY/MM/DD: ")
 
@@ -25,8 +26,14 @@ end_month = int(end_date[1])
 end_day = int(end_date[2])
 end_date = datetime.datetime(end_year, end_month, end_day)
 
+#Add try/except ValueError if input ticker is not available in yahoo finance database
+
 #Add try/except ValueError check here to make sure the input start/end year is within our data bounds 
 #i.e. check 1950?<=Year<=2018); 1<=month<=12; 1<=day<=28/30/31
 
+print("Analyzing " + ticker + " between the dates of " + start + " and " + end + ": ")
 df = web.DataReader([ticker], 'yahoo', start_date, end_date)
 print(df.head(10))
+
+#Visualize the stock's historical movement / volume
+#Compare to other stocks?
